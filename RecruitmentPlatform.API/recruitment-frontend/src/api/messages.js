056842e6e2b26ac16677e0439774c3b77b547ff4
Dewmini Wimodya <dewminiwimodya@gmail.com>
2026-07-22 23:47:28 +0530

    Add comment to messagesApi.getForApplication method
    Added a comment to clarify the purpose of the getForApplication method.

--- RecruitmentPlatform.API/recruitment-frontend/src/api/messages.js
+++ RecruitmentPlatform.API/recruitment-frontend/src/api/messages.js
@@ -1,6 +1,7 @@
 import { api } from "./client";
 
 export const messagesApi = {
+    // Retrieves all messages related to a specific job application.
     getForApplication: (jobApplicationId) => api.get(`/messages?jobApplicationId=${jobApplicationId}`),
     send: (jobApplicationId, body) => api.post("/messages", { jobApplicationId, body }),
 };
