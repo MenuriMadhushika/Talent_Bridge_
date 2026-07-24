c22c4084e7a7d5098f4aa76030c76a09929d9089
Savindya1119 <buddhinisavi@gmail.com>
2026-07-24 07:42:31 +0530

    Update Repository.cs

--- RecruitmentPlatform.API/Repositories/Repository.cs
+++ RecruitmentPlatform.API/Repositories/Repository.cs
@@ -1,1 +1,38 @@
+//RecritmentPlatform.API
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using RecruitmentPlatform.API.Data;
+
+namespace RecruitmentPlatform.API.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate) =>
+            await _dbSet.Where(predicate).ToListAsync();
+
+        public async Task<T?> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate) =>
+            await _dbSet.SingleOrDefaultAsync(predicate);
+
+        public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+        public void Update(T entity) => _dbSet.Update(entity);
+
+        public void Remove(T entity) => _dbSet.Remove(entity);
+
+        public IQueryable<T> Query() => _dbSet.AsQueryable();
+    }
+}
 
